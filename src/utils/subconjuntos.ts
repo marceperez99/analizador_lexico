3b2bfeb5401ff74af94f389e3eb13b2d5e0ddb21
jbust <hugofleitasbus@gmail.com>
2022-05-31 13:12:27 -0400

    Corrige el algoritmo de subconjuntos

--- src/utils/subconjuntos.ts
+++ src/utils/subconjuntos.ts
@@ -1,3 +1,5 @@
+import { FORMERR } from "dns";
+import { off } from "process";
 import { Set } from "typescript";
 import { Automata } from "../types/automata";
 import { Nodo } from "../types/automata";
@@ -7,8 +9,6 @@ function epsilonCerradura(S: Nodo): Set<Nodo> {
   const cerradura = [S];
   let i = 0;
   while (i < cerradura.length) {
-    console.log(cerradura[i]);
-
     const vecinosConVacio = cerradura[i].adyacentes[EPSILON] || [];
     for (let nodo of vecinosConVacio) {
       if (!estaNodoEnLista(cerradura, nodo)) {
@@ -25,7 +25,7 @@ function epsilonCerraduraT(T: Set<Nodo>): Set<Nodo> {
     const epsilon = epsilonCerradura(estado);
     cerradura = unionConjuntos(cerradura, epsilon);
   });
-  return new Set<Nodo>();
+  return cerradura;
 }
 
 function mover(T: Set<Nodo>, a: string): Set<Nodo> {
@@ -36,6 +36,7 @@ function mover(T: Set<Nodo>, a: string): Set<Nodo> {
       resultado.add(adyacente);
     }
   });
+  console.log("Resultado de mover T", resultado);
   return resultado;
 }
 const unionConjuntos = (
@@ -71,12 +72,16 @@ const compararConjuntos = (
   conjuntoA: Set<Nodo>,
   conjuntoB: Set<Nodo>
 ): boolean => {
+  
   if (conjuntoA.size !== conjuntoB.size) return false;
+  let retorno = true;
   conjuntoA.forEach((elem) => {
-    if (!conjuntoB.has(elem)) return false;
+    if (!conjuntoB.has(elem)) {
+        retorno = false;
+    }
   });
 
-  return true;
+  return retorno;
 };
 const estaNodoEnLista = (listaNodos: Nodo[], nodoA: Nodo): boolean => {
   for (let nodoB of listaNodos) {
@@ -96,23 +101,41 @@ export const getAFD = (afn: Automata): Automata => {
   nodos.push(new Nodo(String(0), false));
   let i = 0;
   const alfabeto = afn.alfabeto;
-
+ 
   while (i < Destados.length) {
     let T = Destados[i];
+    console.log("T",T);
     let nodoT = nodos[i];
+    console.log("nodo de T", nodoT)
+
     alfabeto.forEach((caracter) => {
+      console.log("Para el caracter", caracter)
       let U = epsilonCerraduraT(mover(T, caracter));
-      if (!estaConjuntoEnLista(Destados, U)) {
-        Destados.push(U);
-        nodos.push(new Nodo(String(Destados.length), false));
+      console.log("U",U)
+      if(U.size > 0){
+        if (!estaConjuntoEnLista(Destados, U)) {
+            nodos.push(new Nodo(String(Destados.length), false));
+            Destados.push(U);
+          }
+          let index = encontrarConjunto(Destados, U);
+          let nodoU = nodos[index];
+          console.log("nodo de U", nodoU)
+          nodoT.agregarArista(nodoU, caracter);
       }
-      let index = encontrarConjunto(Destados, U);
-      let nodoU = nodos[index];
-      nodoT.agregarArista(nodoU, caracter);
     });
     i++;
   }
   let afd = new Automata(nodos[0]);
+  afd.alfabeto = alfabeto
+  for(let j=0; j < Destados.length; j++){
+    let conjunto = Destados[j]
+    conjunto.forEach(elem => {
+        if(elem.esAceptacion){
+            nodos[j].esAceptacion = true
+        }
+    })
+  }
+  console.log("Nodos",nodos);
+  console.log("Destados",Destados)  
   return afd;
-  //Automata afd = nDestadosew Automata();
 };
