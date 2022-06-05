6a927a16936cb54ac38084c38136181e4f06479d
mperez <marcelo.perez@willdom.com>
2022-06-05 13:50:49 -0400

    Cambia forma en la que se ve la tabla de transiciones

--- src/utils/thompson.ts
+++ src/utils/thompson.ts
@@ -16,6 +16,7 @@ class SimpleLexer {
     this.currentToken = cadena[0];
   }
   nextToken = (): string => {
+    // se mueve en la cadena de entrada
     this.position++;
     this.currentToken = this.cadena[this.position];
     return this.currentToken;
@@ -55,13 +56,17 @@ class RegExpTraductor {
     return caracter;
   };
   expresion = (): ResultadoProduccion => {
+    // Regla: EXPRESION -> OR
     return this.or();
   };
   concat = (): ResultadoProduccion => {
+    // Regla: CONCAT -> KLEENE CONCAT
     const kleene = this.kleene();
     return this.R2(kleene);
   };
+
   R2 = (result: ResultadoProduccion): ResultadoProduccion => {
+    // Regla: R2 -> KLEENE R2
     if (
       this.currentToken &&
       (this.currentToken === "(" ||
@@ -80,10 +85,12 @@ class RegExpTraductor {
     }
   };
   or = (): ResultadoProduccion => {
+    // Regla: OR -> OR R1
     const concat = this.concat();
     return this.R1(concat);
   };
   R1 = (resultado: ResultadoProduccion): ResultadoProduccion => {
+    // Regla: R1 -> '|' R1 | e
     if (this.currentToken === "|") {
       this.match("|");
       const concat = this.concat();
@@ -119,8 +126,10 @@ class RegExpTraductor {
     }
   };
   kleene = (): ResultadoProduccion => {
+    // Regla: KLEENE -> PARENTESIS * | PARENTESIS
     const parentesis = this.parentesis();
 
+    // luego de obtener el resultado de parentesis se usa el metodo hasKleene para verificar si hay un * para indicar la operacion de kleene
     if (this.hasKleene()) {
       const initState = this.estadoCounter++;
       const endState = this.estadoCounter++;
@@ -140,6 +149,7 @@ class RegExpTraductor {
     }
   };
   parentesis = (): ResultadoProduccion => {
+    // Regla: PARENTESIS -> ( EXPRESION ) | CARACTER_O_RANGO
     if (this.currentToken === "(") {
       console.log(this.currentToken);
 
@@ -153,14 +163,18 @@ class RegExpTraductor {
     }
   };
   caracterORango = (): ResultadoProduccion => {
+    // Regla: CARACTER_O_RANGO -> [ RANGO ] | CARACTER
     if (this.currentToken === "[") {
       this.match("[");
-      return this.rango();
+      const rango = this.rango();
+      this.match("]");
+      return rango;
     } else {
       return this.caracter();
     }
   };
   rango = (): ResultadoProduccion => {
+    // Regla: RANGO -> LETRA_MINUSCULA-LETRA_MINUSCULA | LETRA_MAYUSCULA-LETRA_MAYUSCULA | CIFRA-CIFRA
     const nodoInicio = new Nodo(`${this.estadoCounter++}`);
     const nodoFin = new Nodo(`${this.estadoCounter++}`);
     nodoFin.setAceptacion(true, this.clase);
@@ -185,7 +199,6 @@ class RegExpTraductor {
           `Caracter '${caracterFin}' invalido en definicion de rango`
         );
       }
-      this.match("]");
     } else if (this.currentToken >= "A" && this.currentToken <= "Z") {
       const caracterInicio = this.advance();
       this.match("-");
@@ -207,7 +220,6 @@ class RegExpTraductor {
           `Caracter '${caracterFin}' invalido en definicion de rango`
         );
       }
-      this.match("]");
     } else if (this.currentToken >= "0" && this.currentToken <= "9") {
       const caracterInicio = this.advance();
       this.match("-");
@@ -229,13 +241,11 @@ class RegExpTraductor {
           `Caracter '${caracterFin}' invalido en definicion de rango`
         );
       }
-      this.match("]");
     }
     return { nodoFin, nodoInicio };
   };
   caracter = (): ResultadoProduccion => {
-    // TODO: Mejorar deteccion de caracteres
-    // [a-zA-Z] | [0-9] | , | . | \( | \) | *
+    // Regla: CARACTER -> [a-zA-Z] | [0-9] | , | . | \( | \) | \*
 
     if (this.currentToken === "\\") {
       // escape de caracteres especiales
@@ -254,7 +264,9 @@ class RegExpTraductor {
 
     return { nodoInicio, nodoFin };
   };
+
   convert = (): [Nodo, Set<string>] => {
+    // Metodo llamado para conversion de expresion regular a AFN
     const resultado = this.expresion();
     if (this.currentToken !== undefined)
       throw new Error("Caracter inesperado: " + this.currentToken);
@@ -266,6 +278,7 @@ class RegExpTraductor {
 export const parseDefinicion = (
   definicionRegular: string
 ): { clase: string; expresionRegular: string }[] => {
+  // se obtienen los pares: (clase, expresionRegular)
   const reglas = definicionRegular
     .trim()
     .split("\n")
@@ -273,12 +286,13 @@ export const parseDefinicion = (
       let [clase, expresionRegular] = regla
         .split("->")
         .map((cadena) => cadena.trim());
-
+      //Si la clase o la expresion regular es vacia, se lanza error
       if (!clase || !expresionRegular)
         throw new Error("Expresion no valida: " + regla);
       return [clase, expresionRegular];
     });
   const n = reglas.length;
+  // se reemplazan los usos de las clases en las expresiones regulares por las expresiones regulares
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       reglas[j][1] = reglas[j][1].replaceAll(
@@ -287,6 +301,7 @@ export const parseDefinicion = (
       );
     }
   }
+  // si luego de reemplazar n veces cada clase en las expresiones regulares aun se puede hacer un reemplazo, entonces se lanza error indicando una relacion ciclica
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       if (reglas[j][1].includes(`<${reglas[i][0]}>`))
@@ -302,17 +317,18 @@ export const parseDefinicion = (
 };
 
 export const definicionRegularToAFN = (definicion: string): Automata => {
+  //se crea un nodo de inicio con etiqueta 0
   const inicio = new Nodo("0");
   const alfabeto = new Set<string>();
 
   let estadoCounter = 1;
   const producciones = parseDefinicion(definicion);
   for (const { clase, expresionRegular } of producciones) {
-    console.log(clase, expresionRegular);
-
     let traductor = new RegExpTraductor(expresionRegular, estadoCounter, clase);
     let [afn, alfabetoAfn] = traductor.convert();
+    // se conecta el inicio al afn de la expresion regular
     inicio.agregarArista(afn, EPSILON);
+    // se agregan los caracteres  del afn al alfabeto del afn final
     alfabetoAfn.forEach((caracter) => alfabeto.add(caracter));
     estadoCounter = traductor.estadoCounter;
   }
