8fea05d13ca876d368ae2e3f2702e44ed9682ae3
jbust <hugofleitasbus@gmail.com>
2022-06-05 16:31:49 -0400

    agrega comentarios

--- src/utils/minimizacion.ts
+++ src/utils/minimizacion.ts
@@ -1,22 +1,28 @@
 import { Automata } from "../types/automata";
 import { Nodo } from "../types/automata";
-
+//Función que minimiza un AFD
 export function minimizarAFD(afd: Automata): Automata {
   let nodos = obtenerNodos(afd);
   let alfabeto = Array.from(afd.alfabeto);
+  //Divide en subgrupos iniciales
   let subGrupos = obtenerSubgrupos(afd);
   console.log("Subgrupos",subGrupos);
   let cambios = true;
+  //Mientras sea necesario hacer cambios en los subgrupos
   while (cambios) {
     cambios = false;
+    //Para cada subgrupo
     for (let i = 0; i < subGrupos.length; i++) {
       let subGrupo = Array.from(subGrupos[i]);
+      //Para cada caracter
       for (let caracter of alfabeto) {
+        //Obtiene un subgrupo adyacente para comparar
         let primerSubGrupo = getSubGrupo(
           subGrupos,
           subGrupo[0].adyacentes[caracter]?.[0]
         );
         for (let nodo of subGrupo) {
+          //Si es que las aristas van a un subgrupo distinto, separar los subgrupos
           if (
             primerSubGrupo !==
             getSubGrupo(subGrupos, nodo.adyacentes[caracter]?.[0])
@@ -42,14 +48,16 @@ export function minimizarAFD(afd: Automata): Automata {
       }
     }
   }
-  //Marca iniciales y finales
+  
   let nuevosNodos: Nodo[] = [];
   let inicio: Nodo = new Nodo("X");
   for (let subGrupo of subGrupos) {
     let nodo = new Nodo(String(nuevosNodos.length), false);
+    //Marca el estado inicial
     if (subGrupo.has(afd.inicio)) {
       inicio = nodo;
     }
+    //Marca los estados finales con su clase correspondiente
     for (let estado of Array.from(subGrupo)) {
       if (estado.esAceptacion){
           nodo.setAceptacion(true,estado.clase)
@@ -58,17 +66,19 @@ export function minimizarAFD(afd: Automata): Automata {
     nodo.representacion = subGrupo
     nuevosNodos.push(nodo);
   }
-  //Crea el MinAFD y carga las aristas
+  //Conecta los subgrupos entre si
   let minAFD = new Automata(inicio);
   minAFD.alfabeto = afd.alfabeto;
   for (let i = 0; i < nuevosNodos.length; i++) {
     let subGrupo = Array.from(subGrupos[i]);
     let nodo = nuevosNodos[i];
     alfabeto.forEach((caracter) => {
+      //Para cada carácter consigue el subgrupo adyacente
       let index = getSubGrupoIndex(
         subGrupos,
         subGrupo[0].adyacentes[caracter]?.[0]
       );
+      //Agrega la transición correspondiente
       if(index !== -1)
         nodo.agregarArista(nuevosNodos[index], caracter);
     });
@@ -98,10 +108,12 @@ function getSubGrupo(subGrupos: Set<Nodo>[], nodo: Nodo) {
   }
   return undefined;
 }
+//Función que divide en subgrupos para el algoritmo de minimización
 function obtenerSubgrupos(afd: Automata): Set<Nodo>[]{
     let nodos = Array.from(obtenerNodos(afd));
     let subGrupos : Set<Nodo>[] = [];
     let noFinales: Set<Nodo> = new Set<Nodo> ()
+    //Los estados que no son finales pertenecen al primer subgrupo
     for(let nodo of nodos){
         if(!nodo.esAceptacion){
             noFinales.add(nodo);
@@ -110,17 +122,19 @@ function obtenerSubgrupos(afd: Automata): Set<Nodo>[]{
     if (noFinales.size > 0) {
         subGrupos.push(noFinales);
     }
+    //Para los estados de aceptación
     for(let nodo of nodos){
         if(nodo.esAceptacion){
             let agregado = false;
+            //Agrega el estado a un subgrupo en donde todos tienen su misma clase
             for(let subGrupo of subGrupos){
                 let conjuntoLista = Array.from(subGrupo);
-                
                 if(conjuntoLista[0].esAceptacion && conjuntoLista[0].clase === nodo.clase){
                     agregado = true
                     subGrupo.add(nodo);
                 }
             }
+            //Si el subgrupo de esa clase aún no fue creado, crear y agregar el estado al subgrupo
             if(!agregado){
                 let nuevoSubGrupo = new Set<Nodo>()
                 nuevoSubGrupo.add(nodo)
